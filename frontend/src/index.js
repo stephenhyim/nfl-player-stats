90e1737dd33ad7c795fc9b8a0dd7ceea38654b45
Stephen Yim <stephenhyim@gmail.com>
2021-05-13 12:50:37 -0400

    Adding reset button

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -180,77 +180,92 @@ button.addEventListener("click",(e) => {
     window.finalPlayer2 = finalPlayer2
     //********************TESTINGEND******* */
     
-    // if (finalPlayer1[0].position === "QB") 
-    if (selectedPosition = "QB") {
+    if (finalPlayer1[0].position === "QB" && finalPlayer2[0].position === "QB") {
       
         let qbGamesPlayed = [
             {playername: finalPlayer1[0].name, value: finalPlayer1[0].games_played, key: 0},
             {playername: finalPlayer2[0].name, value: finalPlayer2[0].games_played, key: 1}
         ]
         
-        let qbPassingYards = [
-            {playername: finalPlayer1[0].name, value: finalPlayer1[0].passing.yards, key: 0},
-            {playername: finalPlayer2[0].name, value: finalPlayer2[0].passing.yards, key: 1}
-        ]
-
+        
         let qbAttempts = [
             {playername: finalPlayer1[0].name, value: finalPlayer1[0].passing.attempts, key: 0},
             {playername: finalPlayer2[0].name, value: finalPlayer2[0].passing.attempts, key: 1},
             {playername: `${finalPlayer1[0].name} `, value: finalPlayer1[0].passing.completions, key: 0},
             {playername: `${finalPlayer2[0].name} `, value: finalPlayer2[0].passing.completions, key: 1}
         ]
-
+        
         let qbCompletion = [
             {playername: finalPlayer1[0].name, value: finalPlayer1[0].passing.cmp_pct, key: 0},
             {playername: finalPlayer2[0].name, value: finalPlayer2[0].passing.cmp_pct, key: 1}
         ]
 
-        let qbRushing = [
-            {playername: finalPlayer1[0].name, value: finalPlayer1[0].rushing.attempts, key: 0},
-            {playername: finalPlayer2[0].name, value: finalPlayer2[0].rushing.attempts, key: 1},
-            {playername: `${finalPlayer1[0].name} `, value: finalPlayer1[0].rushing.yards, key: 0},
-            {playername: `${finalPlayer2[0].name} `, value: finalPlayer2[0].rushing.yards, key: 1}
+        let qbPassingYards = [
+            {playername: finalPlayer1[0].name, value: finalPlayer1[0].passing.yards, key: 0},
+            {playername: finalPlayer2[0].name, value: finalPlayer2[0].passing.yards, key: 1}
         ]
 
-        let qbTouchDowns = [
+        let qbPassingTD = [
             {playername: finalPlayer1[0].name, value: finalPlayer1[0].passing.touchdowns, key: 0},
             {playername: finalPlayer2[0].name, value: finalPlayer2[0].passing.touchdowns, key: 1},
-            {playername: `${finalPlayer1[0].name} `, value: finalPlayer1[0].rushing.touchdowns, key: 0},
-            {playername: `${finalPlayer2[0].name} `, value: finalPlayer2[0].rushing.touchdowns, key: 1}
         ]
 
         let qbInterceptions = [
             {playername: finalPlayer1[0].name, value: finalPlayer1[0].passing.interceptions, key: 0},
             {playername: finalPlayer2[0].name, value: finalPlayer2[0].passing.interceptions, key: 1}
         ]
+
+        let qbRushingAttempts = [
+            {playername: finalPlayer1[0].name, value: finalPlayer1[0].rushing.attempts, key: 0},
+            {playername: finalPlayer2[0].name, value: finalPlayer2[0].rushing.attempts, key: 1},
+        ]
+
+        let qbRushingYards = [
+            {playername: finalPlayer1[0].name, value: finalPlayer1[0].rushing.yards, key: 0},
+            {playername: finalPlayer2[0].name, value: finalPlayer2[0].rushing.yards, key: 1},
+        ]
+
+        let qbRushingTD = [
+            {playername: finalPlayer1[0].name, value: finalPlayer1[0].rushing.touchdowns, key: 0},
+            {playername: finalPlayer2[0].name, value: finalPlayer2[0].rushing.touchdowns, key: 1}
+        ]
+
         
         let d1Button = document.createElement("button")
         d1Button.onclick = () => {update(qbGamesPlayed)}
         d1Button.textContent = "Games Played"
 
         let d2Button = document.createElement("button")
-        d2Button.onclick = () => {update(qbPassingYards)}
-        d2Button.textContent = "Passing"
+        d2Button.onclick = () => {update(qbAttempts)}
+        d2Button.textContent = "Passing Attempts/Completion"
 
         let d3Button = document.createElement("button")
-        d3Button.onclick = () => {update(qbAttempts)}
-        d3Button.textContent = "Passing Attempts/Completion"
+        d3Button.onclick = () => {update(qbCompletion)}
+        d3Button.textContent = "Passing Completion %"
 
         let d4Button = document.createElement("button")
-        d4Button.onclick = () => {update(qbCompletion)}
-        d4Button.textContent = "Passing Completion %"
+        d4Button.onclick = () => {update(qbPassingYards)}
+        d4Button.textContent = "Passing Yards"
         
         let d5Button = document.createElement("button")
-        d5Button.onclick = () => {update(qbRushing)}
-        d5Button.textContent = "Rushing"
+        d5Button.onclick = () => {update(qbPassingTD)}
+        d5Button.textContent = "Passing TD's"
         
         let d6Button = document.createElement("button")
-        d6Button.onclick = () => {update(qbTouchDowns)}
-        d6Button.textContent = "Touchdowns"
+        d6Button.onclick = () => {update(qbInterceptions)}
+        d6Button.textContent = "Interceptions"
 
         let d7Button = document.createElement("button")
-        d7Button.onclick = () => {update(qbInterceptions)}
-        d7Button.textContent = "Interceptions"
+        d7Button.onclick = () => {update(qbRushingAttempts)}
+        d7Button.textContent = "Rushing Attempts"
+
+        let d8Button = document.createElement("button")
+        d8Button.onclick = () => {update(qbRushingYards)}
+        d8Button.textContent = "Rushing Yards"
+
+        let d9Button = document.createElement("button")
+        d9Button.onclick = () => {update(qbRushingTD)}
+        d9Button.textContent = "Rushing TD's"
 
         let graph = document.getElementsByClassName("graphcontainer")[0]
         // debugger
@@ -261,6 +276,8 @@ button.addEventListener("click",(e) => {
         graph.appendChild(d5Button)
         graph.appendChild(d6Button)
         graph.appendChild(d7Button)
+        graph.appendChild(d8Button)
+        graph.appendChild(d9Button)
 
         const margin = { top: 50, bottom: 50, left: 50, right:50 }
         const width = 800 - margin.left - margin.right;
@@ -278,18 +295,12 @@ button.addEventListener("click",(e) => {
             .range([0, width])
             .padding(0.2);
         const xAxis = svg.append("g")
-            // .attr("transform", "translate(0, " + height + ")")
             .attr("transform", "translate(0, " + height + ")")
         
         const y = d3.scaleLinear()
             .range([height, 0])
         const yAxis = svg.append("g")
             .attr("class", "myYaxis")
-
-        
-       
-        
-        
         
         function update(data) {
 
@@ -314,7 +325,7 @@ button.addEventListener("click",(e) => {
                     .attr("width", x.bandwidth())
                     .attr("height", function(d) {return height - y(d.value); })
                     .attr("fill", function(d) {
-                        debugger
+                        // debugger
                         if (d.key === 0) {
                             return '#e41a1c'
                         } else {
@@ -333,13 +344,193 @@ button.addEventListener("click",(e) => {
 
         // return svg.node();
 
-    }
-    
-    // debugger
+    } else if (finalPlayer1[0].position === "RB" && finalPlayer2[0].position === "RB") {
+        
+        let rbGamesPlayed = [
+            {playername: finalPlayer1[0].name, value: finalPlayer1[0].games_played, key: 0},
+            {playername: finalPlayer2[0].name, value: finalPlayer2[0].games_played, key: 1}
+        ]
 
+        let rbRushingAttempts = [
+            {playername: finalPlayer1[0].name, value: finalPlayer1[0].rushing.attempts, key: 0},
+            {playername: finalPlayer2[0].name, value: finalPlayer2[0].rushing.attempts, key: 1},
+            {playername: `${finalPlayer1[0].name} `, value: finalPlayer1[0].rushing.redzone_attempts, key: 0},
+            {playername: `${finalPlayer2[0].name} `, value: finalPlayer2[0].rushing.redzone_attempts, key: 1}
+        ]
+
+        let rbRushingYards = [
+            {playername: finalPlayer1[0].name, value: finalPlayer1[0].rushing.yards, key: 0},
+            {playername: finalPlayer2[0].name, value: finalPlayer2[0].rushing.yards, key: 1},
+            {playername: `${finalPlayer1[0].name} `, value: finalPlayer1[0].rushing.yards_after_contact, key: 0},
+            {playername: `${finalPlayer2[0].name} `, value: finalPlayer2[0].rushing.yards_after_contact, key: 1}
+        ]
+
+        let rbAvgYards = [
+            {playername: finalPlayer1[0].name, value: finalPlayer1[0].rushing.avg_yards, key: 0},
+            {playername: finalPlayer2[0].name, value: finalPlayer2[0].rushing.avg_yards, key: 1},
+        ]
+
+        let rbRushingTouchDowns = [
+            {playername: finalPlayer1[0].name, value: finalPlayer1[0].rushing.touchdowns, key: 0},
+            {playername: finalPlayer2[0].name, value: finalPlayer2[0].rushing.touchdowns, key: 1},
+        ]
+
+        let rbReceivingTargets = [
+            {playername: finalPlayer1[0].name, value: finalPlayer1[0].receiving.targets, key: 0},
+            {playername: finalPlayer2[0].name, value: finalPlayer2[0].receiving.targets, key: 1},
+            {playername: `${finalPlayer1[0].name} `, value: finalPlayer1[0].receiving.redzone_targets, key: 0},
+            {playername: `${finalPlayer2[0].name} `, value: finalPlayer2[0].receiving.redzone_targets, key: 1}
+        ]
+
+        let rbReceptions = [
+            {playername: finalPlayer1[0].name, value: finalPlayer1[0].receiving.receptions, key: 0},
+            {playername: finalPlayer2[0].name, value: finalPlayer2[0].receiving.receptions, key: 1},
+        ]
+
+        let rbReceivingYards = [
+            {playername: finalPlayer1[0].name, value: finalPlayer1[0].receiving.yards, key: 0},
+            {playername: finalPlayer2[0].name, value: finalPlayer2[0].receiving.yards, key: 1},
+            {playername: `${finalPlayer1[0].name} `, value: finalPlayer1[0].receiving.yards_after_contact, key: 0},
+            {playername: `${finalPlayer2[0].name} `, value: finalPlayer2[0].receiving.yards_after_contact, key: 1}
+        ]
+
+        let rbReceivingTouchDowns = [
+            {playername: finalPlayer1[0].name, value: finalPlayer1[0].receiving.touchdowns, key: 0},
+            {playername: finalPlayer2[0].name, value: finalPlayer2[0].receiving.touchdowns, key: 1},
+        ]
+
+        let d1Button = document.createElement("button")
+        d1Button.onclick = () => {update(rbGamesPlayed)}
+        d1Button.textContent = "Games Played"
+
+        let d2Button = document.createElement("button")
+        d2Button.onclick = () => {update(rbRushingAttempts)}
+        d2Button.textContent = "Rushing Attempts"
+
+        let d3Button = document.createElement("button")
+        d3Button.onclick = () => {update(rbRushingYards)}
+        d3Button.textContent = "Rushing Yards"
+
+        let d4Button = document.createElement("button")
+        d4Button.onclick = () => {update(rbAvgYards)}
+        d4Button.textContent = "Avg Yards"
+        
+        let d5Button = document.createElement("button")
+        d5Button.onclick = () => {update(rbRushingTouchDowns)}
+        d5Button.textContent = "Rushing TD's"
+        
+        let d6Button = document.createElement("button")
+        d6Button.onclick = () => {update(rbReceivingTargets)}
+        d6Button.textContent = "Receiving Targets"
+
+        let d7Button = document.createElement("button")
+        d7Button.onclick = () => {update(rbReceptions)}
+        d7Button.textContent = "Receptions"
+
+        let d8Button = document.createElement("button")
+        d8Button.onclick = () => {update(rbReceivingYards)}
+        d8Button.textContent = "Receiving Yards"
+
+        let d9Button = document.createElement("button")
+        d9Button.onclick = () => {update(rbReceivingTouchDowns)}
+        d9Button.textContent = "Receiving TD's"
+
+        let graph = document.getElementsByClassName("graphcontainer")[0]
+        // debugger
+        graph.appendChild(d1Button)
+        graph.appendChild(d2Button)
+        graph.appendChild(d3Button)
+        graph.appendChild(d4Button)
+        graph.appendChild(d5Button)
+        graph.appendChild(d6Button)
+        graph.appendChild(d7Button)
+        graph.appendChild(d8Button)
+        graph.appendChild(d9Button)
+
+        const margin = { top: 50, bottom: 50, left: 50, right:50 }
+        const width = 800 - margin.left - margin.right;
+        const height = 500 - margin.top - margin.bottom;
+        
+        const svg = d3.select('#d3-container')
+            .append('svg')
+                .attr('height', height + margin.top + margin.bottom)
+                .attr('width', width + margin.left + margin.right )
+            .append("g")
+                .attr("transform", 
+                "translate(" + margin.left + "," + margin.top + ")");
+        
+        const x = d3.scaleBand()
+            .range([0, width])
+            .padding(0.2);
+        const xAxis = svg.append("g")
+            .attr("transform", "translate(0, " + height + ")")
+        
+        const y = d3.scaleLinear()
+            .range([height, 0])
+        const yAxis = svg.append("g")
+            .attr("class", "myYaxis")
+        
+        function update(data) {
+
+            x.domain(data.map(function(d) { return d.playername; }))
+            xAxis.call(d3.axisBottom(x))
+
+            y.domain([0, d3.max(data, function(d) { return d.value }) ]);
+            yAxis.transition().duration(1000).call(d3.axisLeft(y));
+
+            
+            const barChart = svg.selectAll("rect")
+            .data(data)
+
+            barChart
+                .enter()
+                .append("rect")
+                .merge(barChart)
+                .transition()
+                .duration(1000)
+                    .attr("x", function(d) {return x(d.playername); })
+                    .attr("y", function(d) {return y(d.value); })
+                    .attr("width", x.bandwidth())
+                    .attr("height", function(d) {return height - y(d.value); })
+                    .attr("fill", function(d) {
+                        // debugger
+                        if (d.key === 0) {
+                            return '#e41a1c'
+                        } else {
+                            return '#377eb8'
+                        }
+                    })
+                    
+
+            barChart
+                .exit()
+                .remove()
+
+        }
+
+        update(rbGamesPlayed)
+    }
+})
+
+function removeAllChildNodes(parent) {
+    while (parent.firstChild) {
+        parent.removeChild(parent.firstChild);
+    }
+}
+
+const resetbutton = document.getElementById('reset-btn');
+resetbutton.addEventListener("click",(e) => {
+    document.getElementById('position-dropdown').selectedIndex=0;
+    document.getElementById('team1-dropdown').selectedIndex=0;
+    document.getElementById('player1-dropdown').selectedIndex=0;
     
-    
-    
+    const player1dropdown = document.querySelector('#player1-dropdown')
+    removeAllChildNodes(player1dropdown)
+    document.getElementById('team2-dropdown').selectedIndex=0;
+    document.getElementById('player2-dropdown').selectedIndex=0;
+    const player2dropdown = document.querySelector('#player2-dropdown')
+    removeAllChildNodes(player2dropdown)
+    d3.selectAll('svg').remove();
 })
 
 
